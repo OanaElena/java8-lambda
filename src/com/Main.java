3a0040e2072ec3bf52e47cc99be0837a9eeba361
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-02-12 16:19:03 +0200

    employees that has the biggest salaries

--- src/com/Main.java
+++ src/com/Main.java
@@ -11,6 +11,10 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> employeeList = new EmployeeView().createEmployeeList();
         Controller controller = new Controller();
+        System.out.println("1. Return the employee that has the biggest salary: ");
         controller.maxSalary(employeeList).printEmployee();
+        System.out.println("2. Return the employees that has the biggest salaries: ");
+        controller.maxSalaries(employeeList).forEach(Employee::printEmployee);
+        System.out.println("3. Return the employees that has the biggest salaries grouped by departments: ");
     }
 }
--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -1,20 +1,27 @@
 package com.controller;
 
+import com.model.Department;
 import com.model.Employee;
 
-import javax.swing.text.html.Option;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Controller {
 
-    //Find the employer that has the biggest salary from the company, only one
+    //Find the employee that has the biggest salary from the company, only one
 
     public Employee maxSalary(List<Employee> emplSalary){
-        System.out.println("1. Return the employee that has the biggest salary: ");
         Optional<Employee> maxSalaryEmpl = emplSalary.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary)));
         return maxSalaryEmpl.isPresent() ? maxSalaryEmpl.get() : null;
     }
+
+    //Find the employee that has the biggest salary, and if are more than one, show them all
+
+    public List<Employee> maxSalaries(List<Employee> employeeList){
+        Employee maxEmplSalary = maxSalary(employeeList);
+        return employeeList.stream().filter(employee -> employee.getSalary().compareTo(maxEmplSalary.getSalary()) == 0).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    //Find the employee that has the bigest salary grouped by departments
+
 }
